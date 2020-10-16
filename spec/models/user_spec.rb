7fe9a8c0c17e009cddeb9b97542cdd9cd6fc7e68
andre-tlima <andre-tlima@hotmail.com>
2020-10-16 13:35:40 +0000

    Adicionando testes com Shoulda Matchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before { @user = FactoryGirl.build(:user) }
+  let(:user) { build(:user) }
 
-  it { expect(@user).to respond_to(:email) }
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+  it { is_expected.to validate_confirmation_of(:password) }
+  it { is_expected.to allow_value("teste@supergeeks.com.br").for(:email) }
   
 end
